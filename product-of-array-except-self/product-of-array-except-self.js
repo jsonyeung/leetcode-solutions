92e11b705338f2950184210ecb87d6d1e5796d10
Jason Yeung <jasonyeungcontact@gmail.com>
2023-04-27 16:19:57 -0700

    Time: 107 ms (51.28%) | Memory: 55.3 MB (31.63%) - LeetSync

---
+++ product-of-array-except-self/product-of-array-except-self.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let length = nums.length
+    let result = (new Array(length)).fill(1)
+
+    let prefix = 1
+    for (let i = 0; i < length; i++) {
+        result[i] = prefix
+        prefix *= nums[i]
+    }
+
+    let postfix = 1
+    for (let i = length - 1; i >= 0; i--) {
+        result[i] *= postfix
+        postfix *= nums[i]
+    }
+
+    return result
+};
