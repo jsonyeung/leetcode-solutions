9fb69530c4bba14447c37177865b5d58a2e8568c
Jason Yeung <jasonyeungcontact@gmail.com>
2023-05-01 11:45:53 -0700

    Time: 85 ms (38.96%) | Memory: 50 MB (18.99%) - LeetSync

---
+++ course-schedule/course-schedule.js
@@ -0,0 +1,100 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+ /*
+
+Assumptions:
+- all prequisites courses total up to numCourses
+- courses can have no prerequisites whatsoever
+
+2, [1, 0]
+1 <-- 0
+= true (no cycle)
+
+
+3, [[0, 1], [0, 2]]
+0<---1     3
+^
+|
+2
+= true (no cycle)
+
+
+2, [[1, 0], [0, 1]]
+v----v
+0    1
+^----^
+= false (has cycle)
+
+
+4, [[0, 1], [0, 2], [3, 0], [1, 3]]
+------------
+|          v
+0<---1<----3
+^
+|
+2
+= false (cycle)
+
+
+4, [[0, 1], [0, 2], [3, 0]]
+------------
+|          v
+0<---1     3
+^
+|
+2
+= true
+
+"Find if the directed graph has a cycle"
+
+idea:
+Traverse each node to see if we visit the same node backwards (aka, it has a cycle)
+
+ */
+var canFinish = function(numCourses, prerequisites) {
+    let list = {}
+
+    for (let [to, from] of prerequisites) {
+        if (!list[from]) list[from] = []
+        list[from].push(to)
+    }
+
+    // https://www.youtube.com/watch?v=ZS-UPTd73cM
+    // recursive state = values that appear in the recursion stack
+    let visited = []
+    let recState = []
+
+    // checks to see if has cycle
+    function cycleExists(nodeKey) {
+        // console.log(nodeKey, recState[nodeKey])
+        // we are revisting a node already part of recursion stack
+        // therefore, it must be a cycle
+        if (recState[nodeKey]) return true
+
+        // if we visited, we already know this node has no cycle
+        if (visited[nodeKey]) return false
+
+        visited[nodeKey] = true
+        recState[nodeKey] = true
+
+        for (let key of (list[nodeKey] || [])) {
+            if (cycleExists(key)) {
+                return true
+            }
+        }
+
+        recState[nodeKey] = false
+        return false
+    }
+        
+    for (let nodeKey in list) {
+        if (cycleExists(nodeKey)) {
+            return false
+        }
+    }
+
+    return true
+};
