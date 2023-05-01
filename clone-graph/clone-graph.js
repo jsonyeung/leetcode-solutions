01bc523cf3882d4e11565b033f8530143058de41
Jason Yeung <jasonyeungcontact@gmail.com>
2023-05-01 12:14:30 -0700

    Time: 56 ms (86.39%) | Memory: 43.2 MB (94.66%) - LeetSync

--- clone-graph/clone-graph.js
+++ clone-graph/clone-graph.js
@@ -6,30 +6,39 @@
  * };
  */
 
+ /*
+
+Assumption:
+- is connected graph
+
+DFS while keeping track of visited nodes
+    check all neighbours of current node
+        if neigbour was not visited, DFS
+
+ */
+
 /**
  * @param {Node} node
  * @return {Node}
  */
 var cloneGraph = function(node) {
-    if (node == null) return null
+    if (node == null) return node
+    let visited = new Map()
 
-    let cloneMap = new Map()
-
-    function cloneNode(node) {
-        let clone = cloneMap.get(node)
-        if (clone != null) return clone
-
-        // if clone does not exist, create a clone...
-        clone = new Node(node.val)
-        cloneMap.set(node, clone)
-
-        // ...and assign its cloned neighbours based on current node
-        for (let neighbor of node.neighbors) {
-            clone.neighbors.push(cloneNode(neighbor))
+    function dfs(node) {
+        if (visited.has(node)) {
+            return visited.get(node)
         }
 
-        return clone
+        let newNode = new Node(node.val)
+        visited.set(node, newNode)
+
+        for (let neighbor of node.neighbors) {
+            newNode.neighbors.push(dfs(neighbor))
+        }
+
+        return newNode
     }
 
-    return cloneNode(node)
+    return dfs(node)
 };
