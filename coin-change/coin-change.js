6805e20b39be951a2b55dad0fed99853a7dd3c4b
Jason Yeung <jasonyeungcontact@gmail.com>
2023-05-05 21:57:42 -0700

    Time: 109 ms (66.19%) | Memory: 47.5 MB (55.98%) - LeetSync

--- coin-change/coin-change.js
+++ coin-change/coin-change.js
@@ -3,30 +3,18 @@
  * @param {number} amount
  * @return {number}
  */
-
- /*
-
-[1, 10, 9] 10 = 1
-
-[5, 1, 2] 10 = 2 (5 + 5)
-
-[5, 2] 1 = 0
-
-[1 3 4 5] 7 = 2 (3 + 4)
-Can't go greedy bc (5 + 1 + 1) = 3
-
- */
 var coinChange = function(coins, amount) {
+    // bottom-up (start w/ smallest solution, and build upwards)
     let dp = Array(amount + 1).fill(Infinity)
     dp[0] = 0
 
     for (let a = 1; a <= amount; a++) {
         for (let c of coins) {
-            if ((a - c) < 0) continue
+            let diff = a - c
 
-            // dp[a - c] = the optimal # of coins of the remaining amount after taking out c
-            // "1 +" = what if we adding this coin to the solution
-            dp[a] = Math.min(dp[a], 1 + dp[a - c])
+            if (diff >= 0) {
+                dp[a] = Math.min(dp[a], 1 + dp[diff])
+            }
         }
     }
 
