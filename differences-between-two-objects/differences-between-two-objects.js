dd6b254c4cca3a364b10f18d13ab4c6efb80c97f
Jason Yeung <jasonyeungcontact@gmail.com>
2023-06-04 11:31:25 -0700

    Time: 62 ms (62.13%) | Memory: 43.8 MB (52.88%) - LeetSync

--- differences-between-two-objects/differences-between-two-objects.js
+++ differences-between-two-objects/differences-between-two-objects.js
@@ -3,27 +3,6 @@
  * @param {object} obj2
  * @return {object}
  */
- /*
-
- results = {}
-
-checkProperty(pointer = results)
- for a given property A
-    if typeof obj1[A] and obj2[A] are not same
-        add obj1[A] and obj2[A] diff to pointer
-    
-    if obj1[A] and obj2[A] are same type
-        if obj1[A] (and obj2[A]) is object or array
-            add key results[A] = {}
-            pointer = results[A]
-
-            loop through keys
-                pointer[key] = checkProperty(pointer)
-        
-        else if obj1[A] !== obj2[A]
-            add obj1[A] and obj2[A] diff to pointer
-
- */
 function objDiff(obj1, obj2) {
     if (obj1 === obj2) return {}
 
