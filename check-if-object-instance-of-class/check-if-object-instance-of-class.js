7451fccb9111b82d2d154935d0b4ff38b1b08f3b
Jason Yeung <jasonyeungcontact@gmail.com>
2023-06-06 15:16:23 -0700

    Time: 112 ms (38.20%) | Memory: 52.1 MB (39.63%) - LeetSync

---
+++ check-if-object-instance-of-class/check-if-object-instance-of-class.js
@@ -0,0 +1,26 @@
+/**
+ * @param {any} obj
+ * @param {any} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function(obj, classFunction) {
+    if (obj == null || typeof classFunction !== 'function') {
+        return false
+    }
+
+    let prototype = Object.getPrototypeOf(obj)
+
+    while (prototype != null) {
+        if (prototype === classFunction.prototype) {
+            return true
+        }
+
+        prototype = Object.getPrototypeOf(prototype)
+    }
+
+    return false
+};
+
+/**
+ * checkIfInstanceOf(new Date(), Date); // true
+ */
