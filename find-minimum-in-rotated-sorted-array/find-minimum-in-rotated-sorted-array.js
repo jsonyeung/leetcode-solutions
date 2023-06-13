73b74ffbf48af4fe70aeac950a60f1bce9663a6d
Jason Yeung <jasonyeungcontact@gmail.com>
2023-06-12 17:15:05 -0700

    Time: 61 ms (45.61%) | Memory: 41.9 MB (62.67%) - LeetSync

--- find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
+++ find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
@@ -4,11 +4,31 @@
  */
 var findMin = function(nums) {
     const findPivotNumber = () => {
-        for (let i = 1; i < nums.length; i++) {
-            if (nums[i] < nums[i-1]) {
-                return nums[i]
+        if (nums.length === 1) {
+            return nums[0]
+        }
+
+        let l = 0, r = (nums.length - 1)
+
+        while (l < r) {
+            let mid = l + Math.floor((r - l) / 2)
+
+            // check if pivot exists near mid
+            if (nums[mid + 1] < nums[mid]) {
+                return nums[mid + 1]
+            } else if (nums[mid] < nums[mid - 1]) {
+                return nums[mid]
+            } 
+
+            // if not pivot exists in current mid, 
+            // move left or right relative to the first value
+            if (nums[mid] > nums[0]) {
+                l = mid + 1
+            } else {
+                r = mid - 1
             }
         }
+
         return nums[0]
     }
 
