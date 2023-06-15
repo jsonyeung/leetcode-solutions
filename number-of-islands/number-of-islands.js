4505749669142c5661b6b5c8004bda73541c112f
Jason Yeung <jasonyeungcontact@gmail.com>
2023-06-14 17:35:20 -0700

    Time: 183 ms (7.78%) | Memory: 64.2 MB (5.19%) - LeetSync

--- number-of-islands/number-of-islands.js
+++ number-of-islands/number-of-islands.js
@@ -2,65 +2,45 @@
  * @param {character[][]} grid
  * @return {number}
  */
-/*
-    Assumptions:
-    - Can have no islands at all
-    - Two islands are considered separate if their tiles are not connected adjacently
-
-
-visited = set()
-islands = 0
-
-search each tile on grid
-- if tile is a 1 and not visited, run DFS
-    assign all tiles as visited after DFS
-    islands += 1
-
-*/
 var numIslands = function(grid) {
     let visited = new Set()
 
-    function getValidNeighbours(r, c) {
-        let dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-        let neighbours = []
-
-        for (let [dr, dc] of dirs) {
-            let nr = r + dr
-            let nc = c + dc
-
-            if (nr < 0 || nr >= grid.length) continue
-            if (nc < 0 || nc >= grid[0].length) continue
-
-            if (grid[nr][nc] === '0') continue
-            if (visited.has([nr, nc].toString())) continue
-
-            neighbours.push([nr, nc])
-        }
-
-        return neighbours
+    function wasVisited(row, col) {
+        return (visited.has(`${row}, ${col}`))
     }
 
-    function dfs(r, c) {
-        let validNeighbours = getValidNeighbours(r, c)
+    function addToVisited(row, col) {
+        visited.add(`${row}, ${col}`)
+    }
 
-        if (validNeighbours <= 0) return
+    function traverse(row, col) {
+        if (wasVisited(row, col)) return
+        if (row < 0 || row >= grid.length) return
+        if (col < 0 || col >= grid[0].length) return
+        if (grid[row][col] === '0') return
 
-        for (let [nr, nc] of validNeighbours) {
-            visited.add([nr, nc].toString())
-            dfs(nr, nc)
+        addToVisited(row, col)
+
+        const dir = [
+            [1, 0], [-1, 0], 
+            [0, -1], [0, 1]
+        ]
+
+        for (const [dr, dc] of dir) {
+            traverse(row + dr, col + dc)
         }
     }
 
-    let numIslands = 0
+    let blobs = 0
 
-    for (let r = 0; r < grid.length; r++) {
-        for (let c = 0; c < grid[0].length; c++) {
-            if (grid[r][c] === '1' && !visited.has([r, c].toString())) {
-                dfs(r, c)
-                numIslands += 1
+    for (let row = 0; row < grid.length; row++) {
+        for (let col = 0; col < grid[0].length; col++) {
+            if (!wasVisited(row, col) && (grid[row][col] === '1')) {
+                traverse(row, col)
+                blobs++
             }
         }
     }
 
-    return numIslands
+    return blobs
 };
