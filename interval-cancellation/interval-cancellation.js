388a33aba58cba8963abf153b5f07cbe0039618a
Jason Yeung <jasonyeungcontact@gmail.com>
2023-07-02 21:28:02 -0700

    Time: 71 ms (56.25%) | Memory: 42.5 MB (8.96%) - LeetSync

---
+++ interval-cancellation/interval-cancellation.js
@@ -0,0 +1,47 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    fn.apply(null, args)
+
+    let isCanceled = false
+    let loop = setInterval(() => {
+        if (isCanceled) clearInterval(loop)
+        else fn.apply(null, args)
+    }, t)
+
+    return () => {
+        isCanceled = true
+    }
+};
+
+/**
+ *  const result = []
+ *
+ *  const fn = (x) => x * 2
+ *  const args = [4], t = 20, cancelT = 110
+ *
+ *  const start = performance.now()
+ *
+ *  const log = (...argsArr) => {
+ *		const val = fn(...argsArr)
+ *      result.push({"time": Math.floor(performance.now() - start), "returned": fn(...argsArr)})
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *           
+ *  setTimeout(() => {
+ *     cancel()
+ *     console.log(result) // [
+ *                         //      {"time":0,"returned":8},
+ *                         //      {"time":20,"returned":8},
+ *                         //      {"time":40,"returned":8},           
+ *                         //      {"time":60,"returned":8},
+ *                         //      {"time":80,"returned":8},
+ *                         //      {"time":100,"returned":8}
+ *                         //  ]
+ *  }, cancelT)
+ */
