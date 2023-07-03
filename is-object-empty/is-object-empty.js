5e197aec260d1c4b350390beb93ef2ca6bb8894f
Jason Yeung <jasonyeungcontact@gmail.com>
2023-07-02 21:10:06 -0700

    Time: 66 ms (16.76%) | Memory: 44.1 MB (5.77%) - LeetSync

---
+++ is-object-empty/is-object-empty.js
@@ -0,0 +1,10 @@
+/**
+ * @param {Object | Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    return (
+        JSON.stringify(obj) === '{}' ||
+        JSON.stringify(obj) === '[]'
+    )
+};
