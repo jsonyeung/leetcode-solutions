d909e0028ecb80f87e4192cbe18c1e580d1f19d0
Jason Yeung <jasonyeungcontact@gmail.com>
2023-08-02 11:23:48 -0700

    Time: 56 ms (96.93%) | Memory: 45.5 MB (15.14%) - LeetSync

--- 46-permutations/permutations.js
+++ 46-permutations/permutations.js
@@ -5,21 +5,20 @@
 var permute = function(nums) {
     const results = []
 
-    function dfs(sub = [], i = 0, used = []) {
+    function dfs(sub = [], used = []) {
         if (sub.length >= nums.length) {
             results.push([...sub])
         }
 
         for (let j = 0; j < nums.length; j++) {
-            if (!used[j]) {
-                sub.push(nums[j])
-                used[j] = true
-                
-                dfs(sub, j, used)
-                
-                sub.pop()
-                used[j] = false
-            }
+            if (used[j]) continue
+            
+            sub.push(nums[j])
+            used[j] = true
+            
+            dfs(sub, used)
+            sub.pop()
+            used[j] = false
         }
     }
 
