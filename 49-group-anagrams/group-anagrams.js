a7953f6da4cf7fa818791fc3ac2f9b1e2decb524
Jason Yeung <jasonyeungcontact@gmail.com>
2024-01-17 19:28:38 -0800

    Time: 123 ms (33.83%) | Memory: 57 MB (7.03%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+/*
+
+map
+
+for each string,
+    get the key = sorted string
+
+    if (map[key] doesnt exist)
+        map[key] = []
+
+    push string into map[key]
+
+return the values of map
+
+
+*/
+var groupAnagrams = function(strs) {
+    let map = new Map()
+
+    for (let str of strs) {
+        let key = str.split('')
+            .sort((a, b) => a.localeCompare(b)).join('')
+
+        if (!map.has(key)) map.set(key, [])
+        map.set(key, [...map.get(key), str])
+    }
+
+    return Array.from(map.values())
+};
