8e50340355a3e75f2effd0dbbd0f339289e0a3e8
Jason Yeung <jasonyeungcontact@gmail.com>
2024-02-22 19:55:29 -0800

    Time: 74 ms (21.12%) | Memory: 52.3 MB (8.00%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.js
+++ 1127-last-stone-weight/last-stone-weight.js
@@ -2,20 +2,41 @@
  * @param {number[]} stones
  * @return {number}
  */
-var lastStoneWeight = function(stones) {
-    let remaining = stones.slice()
+ /*
 
-    while (remaining.length > 1) {
-        remaining = remaining.sort((a, b) => a - b)
-        remaining.push(Math.abs(remaining.pop() - remaining.pop()))
+ [1]
+
+ init and push elements into heap
+
+
+ while (heap.size is > 1) {
+     pop two elements off, smash them
+
+     if smashed diff is 0 do nothing
+     else push back into heap
+ }
+
+ if (heap.size is 1) return heap.pop
+
+ else return 0
+ 
+time: O(n logn)
+space: O(n)
+
+ */
+var lastStoneWeight = function(stones) {
+    const maxQueue = new MaxPriorityQueue();
+    stones.forEach((stone) => maxQueue.enqueue(stone))
+
+    while (maxQueue.size() > 1) {
+        let stoneA = maxQueue.dequeue().element
+        let stoneB = maxQueue.dequeue().element
+        let diff = (stoneA - stoneB)
+
+        if (diff !== 0) {
+            maxQueue.enqueue(diff)
+        }
     }
 
-    return remaining
+    return (maxQueue.size() === 1) ? maxQueue.dequeue().element : 0
 };
-
-
-/*
-
-[8, 7, 4, 2, 1, 1]
-[1, 4, 1, 1]
-*/
