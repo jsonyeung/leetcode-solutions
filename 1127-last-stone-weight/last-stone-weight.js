a5c4585277a20003185349689476565a86ba9848
Jason Yeung <jasonyeungcontact@gmail.com>
2024-02-24 09:13:47 -0800

    Time: 61 ms (53.11%) | Memory: 50.5 MB (34.45%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.js
+++ 1127-last-stone-weight/last-stone-weight.js
@@ -2,41 +2,18 @@
  * @param {number[]} stones
  * @return {number}
  */
- /*
-
- [1]
-
- init and push elements into heap
-
-
- while (heap.size is > 1) {
-     pop two elements off, smash them
-
-     if smashed diff is 0 do nothing
-     else push back into heap
- }
-
- if (heap.size is 1) return heap.pop
-
- else return 0
- 
-time: O(n logn)
-space: O(n)
-
- */
 var lastStoneWeight = function(stones) {
-    const maxQueue = new MaxPriorityQueue();
-    stones.forEach((stone) => maxQueue.enqueue(stone))
+    let sorted = stones.sort((a, b) => b - a)
 
-    while (maxQueue.size() > 1) {
-        let stoneA = maxQueue.dequeue().element
-        let stoneB = maxQueue.dequeue().element
-        let diff = (stoneA - stoneB)
+    while (sorted.length > 1) {
+        let stoneB = sorted.shift()
+        let stoneA = sorted.shift()
 
-        if (diff !== 0) {
-            maxQueue.enqueue(diff)
+        if (stoneB !== stoneA) {
+            sorted.push(stoneB - stoneA)
+            sorted.sort((a, b) => b - a)
         }
     }
 
-    return (maxQueue.size() === 1) ? maxQueue.dequeue().element : 0
+    return sorted[0] || 0
 };
