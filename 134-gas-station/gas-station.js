d007544a120e726dd6d8ae705d572fa45b9c6b88
Jason Yeung <mail.jason.yeung@gmail.com>
2024-03-02 08:49:47 -0800

    Time: 75 ms (62.47%) | Memory: 60.7 MB (60.93%) - LeetSync

--- 134-gas-station/gas-station.js
+++ 134-gas-station/gas-station.js
@@ -4,25 +4,22 @@
  * @return {number}
  */
 var canCompleteCircuit = function(gas, cost) {
+    let totalSum = 0
+
     let runningSum = 0
     let startingStation = 0
 
-    let totalGas = 0
-    let totalCost = 0
-
     for (let i = 0; i < gas.length; i++) {
-        totalGas += gas[i]
-        totalCost += cost[i]
-
-        runningSum += gas[i] - cost[i]
+        totalSum += (gas[i] - cost[i])
+        runningSum += (gas[i] - cost[i])
 
         if (runningSum < 0) {
             runningSum = 0
-            startingStation = i + 1
+            startingStation = i+1
         }
     }
 
-    if (totalGas < totalCost) {
+    if (totalSum < 0) {
         return -1
     }
 
