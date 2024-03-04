430f731fe99fa08fd5367641c250e5042f15b18e
Jason Yeung <mail.jason.yeung@gmail.com>
2024-03-03 17:42:20 -0800

    Time: 58 ms (73.30%) | Memory: 51.5 MB (55.46%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.js
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -4,18 +4,16 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
+    let stack = [nums2[0]]
     let map = {}
-    let stack = []
 
-    for (let num of nums2) {
-        while (stack.length > 0 && stack[stack.length - 1] < num) {
-            map[stack.pop()] = num
+    for (let i = 1; i < nums2.length; i++) {
+        while (stack.length > 0 && stack[stack.length - 1] < nums2[i]) {
+            map[stack.pop()] = nums2[i]
         }
 
-        stack.push(num)
+        stack.push(nums2[i])
     }
 
-    return nums1.map((value) => {
-        return map[value] ?? -1
-    })
+    return nums1.map((value) => map[value] || -1)
 };
