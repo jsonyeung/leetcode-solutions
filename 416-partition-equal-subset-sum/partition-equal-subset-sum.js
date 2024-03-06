4e911a83932c45020e5ff744be9d127eb21b9be6
Jason Yeung <mail.jason.yeung@gmail.com>
2024-03-05 19:57:33 -0800

    Time: 1297 ms (9.17%) | Memory: 176.9 MB (5.09%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.js
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.js
@@ -4,34 +4,40 @@
  */
 var canPartition = function(nums) {
     let total = nums.reduce((acc, val) => acc + val, 0)
-    let memo = {}
 
     if (total % 2 !== 0) {
         return false
     }
 
-    function helper(i = 0, target) {
+    let target = total / 2
+    let memo = {}
+
+    function helper(i, target) {
         let key = `${i}_${target}`
 
-        if (target === 0) {
-            return true
+        if (i >= nums.length) {
+            return false
         }
 
         if (target < 0) {
             return false
         }
 
-        if (i >= nums.length) {
-            return false
+        if (target === 0) {
+            return true
         }
 
         if (memo[key] != null) {
             return memo[key]
         }
 
-        memo[key] = helper(i + 1, target - nums[i]) || helper(i + 1, target)
+        memo[key] = (
+            helper(i+1, target - nums[i]) ||
+            helper(i+1, target)
+        )
+
         return memo[key]
     }
 
-    return helper(0, total / 2)
+    return helper(0, target)
 };
