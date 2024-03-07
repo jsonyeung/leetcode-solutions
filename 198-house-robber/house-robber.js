2fc5e9206da5602b6b8ca2e90c2967656cfeaf95
Jason Yeung <mail.jason.yeung@gmail.com>
2024-03-07 11:13:16 -0800

    Time: 52 ms (54.27%) | Memory: 48.8 MB (50.51%) - LeetSync

--- 198-house-robber/house-robber.js
+++ 198-house-robber/house-robber.js
@@ -2,33 +2,24 @@
  * @param {number[]} nums
  * @return {number}
  */
- /*
-
- money = 0, i = 0
-
- rob this house and skip next house
-
- skip and move to the next house
-
- */
 var rob = function(nums) {
     let memo = {}
 
-    function helper(i = 0, money = 0) {
+    function helper(i = 0) {
         if (i >= nums.length) {
-            return money
+            return 0
         }
 
-        let key = [i, money].join('_')
-        
-        if (memo[key] != null) {
-            return memo[key]
+        if (memo[i] != null) {
+            return memo[i]
         }
 
-        return memo[key] = Math.max(
-            helper(i+2, money + nums[i]),
-            helper(i+1, money)
+        memo[i] = Math.max(
+            nums[i] + helper(i+2),
+            helper(i+1)
         )
+
+        return memo[i]
     }
 
     return helper()
