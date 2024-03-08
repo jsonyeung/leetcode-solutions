5ef43e6891a3d946a8f008b59319ac1f8ad66904
Jason Yeung <mail.jason.yeung@gmail.com>
2024-03-08 13:56:29 -0800

    Time: 73 ms (67.18%) | Memory: 53.5 MB (34.65%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/set-matrix-zeroes.js
@@ -3,23 +3,28 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function(matrix) {
-    let stack = []
+    let row = []
+    let col = []
 
     for (let r = 0; r < matrix.length; r++) {
         for (let c = 0; c < matrix[0].length; c++) {
             if (matrix[r][c] === 0) {
-                stack.push([r, c])
+                row[r] = true
+                col[c] = true
             }
         }
     }
 
-    while (stack.length > 0) {
-        let [r, c] = stack.pop()
-
+    for (let r = 0; r < matrix.length; r++) {
+        if (!row[r]) continue
         matrix[r] = new Array(matrix[0].length).fill(0)
+    }
 
-        for (let dr = 0; dr < matrix.length; dr++) {
-            matrix[dr][c] = 0
+    for (let c = 0; c < matrix[0].length; c++) {
+        if (!col[c]) continue
+        
+        for (let r = 0; r < matrix.length; r++) {
+            matrix[r][c] = 0
         }
     }
 
