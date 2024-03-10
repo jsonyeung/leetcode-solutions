e4740b4e1f82a2d49375fbac8e719b02d5b29841
Jason Yeung <mail.jason.yeung@gmail.com>
2024-03-10 11:30:25 -0700

    Time: 147 ms (86.87%) | Memory: 66.6 MB (15.01%) - LeetSync

---
+++ 15-3sum/3sum.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    nums = nums.sort((a, b) => a - b)
+
+    let results = new Set()
+
+    for (let i = 0; i < nums.length; i++) {
+        let left = i
+        let cur = (left + 1)
+        let right = (nums.length - 1)
+
+        // avoid duplicates
+        if (nums[i-1] === nums[left]) {
+            continue
+        }
+
+        while (cur < right) {
+            let sum = nums[left] + nums[cur] + nums[right]
+
+            if (sum === 0) {
+                results.add([nums[left], nums[cur], nums[right]])
+                cur++; right--
+                
+                // avoid duplicates
+                while (nums[cur-1] === nums[cur]) cur++
+                
+
+            } else if (sum < 0) {
+                cur++
+
+            } else {
+                right--
+            }
+        }
+    }
+
+    return Array.from(results)
+};
