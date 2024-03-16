87002e16ea2531103df23113caf7b76b427de6bb
Jason Yeung <mail.jason.yeung@gmail.com>
2024-03-16 16:34:53 -0700

    Time: 56 ms (58.36%) | Memory: 49.9 MB (88.61%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
@@ -10,22 +10,49 @@
  * @param {number} n
  * @return {ListNode}
  */
+/*
+
+edge case: if no nodes return null
+
+edge case: if 1 node, n = 1
+return null
+
+edge case: if the node that needs to be removed is the head
+
+1 -> 2, n = 2
+
+s -> 1 -> 2
+^     ^
+          ^
+
+2
+
+*/
 var removeNthFromEnd = function(head, n) {
-    if (head.next == null) return null
+    if (head == null || head.next == null) {
+        return null
+    }
 
     let sentinel = new ListNode(null, head)
-    let start = sentinel
-    let end = head
+
+    let prev = sentinel
+    let start = sentinel.next
+    let end = sentinel
 
     for (let i = 0; i < n; i++) {
         end = end.next
     }
 
-    while (end != null) {
-        end = end.next
+    // console.log(head, prev.val, start.val, end.val)
+
+    while (end.next) {
+        prev = prev.next
         start = start.next
+        end = end.next
     }
 
-    start.next = start.next.next
+    // console.log(head, prev.val, start.val, end.val)
+
+    prev.next = start.next
     return sentinel.next
 };
