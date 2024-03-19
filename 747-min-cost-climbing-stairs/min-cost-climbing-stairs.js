970934b5e9d96c23a1a5e7cb416a3dd524110780
Jason Yeung <mail.jason.yeung@gmail.com>
2024-03-18 21:36:56 -0700

    Time: 54 ms (73.42%) | Memory: 50.9 MB (15.79%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.js
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.js
@@ -2,46 +2,23 @@
  * @param {number[]} cost
  * @return {number}
  */
- /*
-
-
- [2, 1, 4, 5, 0]
-
-memo = {
-    0: 2
-    1: 1
-    3: 5
-    4: 6
-    5: 5
-}
-
-memo[step] = Math.min(1000 + 1, 1000 + 10)
-
-step = 1
-
-
- cost = 1           step = 1
-      /    \
-    11    1000      step = 2
-  /  \     /   \ 
-1011 15  1005 1000   step = 3
-/  \ 
-1016 1011
- */
-
-
 var minCostClimbingStairs = function(cost) {
-    let costs = [...cost, 0]
-    let memo = { 0: cost[0], 1: cost[1] }
-    let step = 2
+    let memo = {}
 
-    while (step < costs.length) {
-        memo[step] = Math.min(
-            costs[step] + memo[step - 1], 
-            costs[step] + memo[step - 2]
+    function helper(i = 0) {
+        if (i >= cost.length) {
+            return 0
+        }
+
+        if (memo[i] != null) {
+            return memo[i]
+        }
+
+        return memo[i] = cost[i] + Math.min(
+            helper(i+1),
+            helper(i+2)
         )
-        step++
     }
 
-    return memo[costs.length - 1]
+    return Math.min(helper(0), helper(1))
 };
