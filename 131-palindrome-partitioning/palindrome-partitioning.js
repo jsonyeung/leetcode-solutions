5750aa7aa9ab6691da0c92ad226e5d58cc3f0474
Jason Yeung <mail.jason.yeung@gmail.com>
2024-03-23 21:30:37 -0700

    Time: 595 ms (5.08%) | Memory: 73.6 MB (81.47%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {string[][]}
+ */
+var partition = function(s) {
+    let results = []
+    let partition = []
+
+    function isPalindrome(string) {
+        return (string.split('').reverse().join('') === string)
+    }
+
+    function helper(i = 0) {
+        if (i >= s.length) {
+            results.push([...partition])
+            return
+        }
+
+        for (let j = i + 1; j <= s.length; j++) {
+            let current = s.slice(i, j)
+
+            console.log(current, isPalindrome(current))
+
+            if (isPalindrome(current)) {
+                partition.push(current)
+                helper(j)
+                partition.pop()
+            }
+        }
+    }
+
+    helper()
+    return results
+};
