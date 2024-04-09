92af2cb5bbd78347f040f3aa4726456e247457f8
Jason Yeung <mail.jason.yeung@gmail.com>
2024-04-08 20:00:16 -0700

    Time: 69 ms (48.15%) | Memory: 57.5 MB (28.83%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.js
+++ 11-container-with-most-water/container-with-most-water.js
@@ -2,28 +2,15 @@
  * @param {number[]} height
  * @return {number}
  */
- /*
-
-move left until it is greater than right
-
-move right until it is greater than left
-
-keep max area along the way
-
-repeat until left >= right
-
- */
 var maxArea = function(height) {
     let left = 0
     let right = (height.length - 1)
-
     let maxArea = -Infinity
-    console.log(left, right)
 
     while (left < right) {
-        let area = (right - left) * Math.min(height[left], height[right])
+        let area = Math.min(height[left], height[right]) * (right - left)
 
-        maxArea = Math.max(area, maxArea)
+        maxArea = Math.max(maxArea, area)
 
         if (height[left] <= height[right]) {
             left++
