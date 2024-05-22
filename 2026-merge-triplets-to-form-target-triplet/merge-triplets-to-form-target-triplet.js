dea3ba811dca4478e687f6a069ebe850d6559646
Jason Yeung <mail.jason.yeung@gmail.com>
2024-05-22 12:12:38 -0700

    Time: 149 ms (72.46%) | Memory: 80.1 MB (53.62%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.js
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.js
@@ -4,42 +4,19 @@
  * @return {boolean}
  */
 var mergeTriplets = function(triplets, target) {
-    
-    /* 
-        has number = set()
-        for each number in target
-            for eahc in triplet
-                if triplet contains value > target value,
-                    ignore it and continue
-
-                if number === in triplet
-                    add index to set
-                
-                if number <= triplet
-                    push to remaining triplets
-        
-        return true if set has 3 indices
-    */
-
-    let hasNumber = new Set()
+    let set = new Set()
 
     for (let i = 0; i < target.length; i++) {
-        let remainingTriplets = []
-
         for (let triplet of triplets) {
             if (triplet[0] > target[0] || triplet[1] > target[1] || triplet[2] > target[2]) {
-                continue
+                continue                
             }
 
             if (triplet[i] === target[i]) {
-                hasNumber.add(i)
-            }
-
-            if (triplet[i] <= target[i]) {
-                remainingTriplets.push(triplet)
+                set.add(i)
             }
         }
     }
 
-    return (hasNumber.size === 3)
+    return (set.size === 3)
 };
