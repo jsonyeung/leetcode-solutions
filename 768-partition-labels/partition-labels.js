954517b94b8e07bc3bc465378389bdffa92b35c0
Jason Yeung <mail.jason.yeung@gmail.com>
2024-05-23 08:45:38 -0700

    Time: 78 ms (11.06%) | Memory: 51.5 MB (45.37%) - LeetSync

--- 768-partition-labels/partition-labels.js
+++ 768-partition-labels/partition-labels.js
@@ -3,31 +3,33 @@
  * @return {number[]}
  */
 var partitionLabels = function(s) {
-    let count = {}
+    let map = {}
 
     for (let char of s) {
-        count[char] = (count[char] || 0) + 1
+        map[char] = (map[char] || 0) + 1
     }
 
-    let visited = new Set()
-    let results = []
-
+    let seen = new Set()
     let remaining = 0
+    let partitions = []
+
     let start = 0
 
     for (let i = 0; i < s.length; i++) {
-        if (!visited.has(s[i])) {
-            remaining += count[s[i]]
-            visited.add(s[i])
+        let char = s[i]
+
+        if (!seen.has(s[i])) {
+            seen.add(s[i])
+            remaining += map[s[i]]
         }
         
         remaining--
 
-        if (remaining === 0) {
-            results.push(i - start + 1)
+        if (remaining <= 0) {
+            partitions.push(i - start + 1)
             start = i + 1
         }
     }
 
-    return results
+    return partitions
 };
