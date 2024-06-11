1ff3bee517c637be0c8e8d8efe14dba151724cdf
Jason Yeung <mail.jason.yeung@gmail.com>
2024-06-11 13:06:15 -0700

    Time: 65 ms (85.31%) | Memory: 58.3 MB (22.36%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.js
+++ 53-maximum-subarray/maximum-subarray.js
@@ -2,12 +2,20 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
-    let runningSum = 0
-    let maxSum = -Infinity
+ /*
 
-    for (let i = 0; i < nums.length; i++) {
-        runningSum += nums[i]
+ [10 -100 99] // [99]
+ [99 -100 10] // [99]
+ [100 -100 101] // [100 -100 101]
+ [-10 100 -100 101 -10] // [100 -100 101]
+
+ */
+var maxSubArray = function(nums) {
+    let maxSum = nums[0]
+    let runningSum = 0
+
+    for (let num of nums) {
+        runningSum += num
         maxSum = Math.max(maxSum, runningSum)
 
         if (runningSum < 0) {
