f85b8da1b6829ac6eff105502c4fe2c7e6bac0a7
Jason Yeung <mail.jason.yeung@gmail.com>
2024-06-11 11:04:04 -0700

    Time: 68 ms (8.53%) | Memory: 50.4 MB (56.47%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
@@ -19,9 +19,9 @@ var removeNthFromEnd = function(head, n) {
     S -> 1 -> 2 -> 3 -> 4 -> 5, n = 2    |   S -> 1 -> 2 -> 3 -> 4 -> 5, n = 2
     s    n    e                          |                  s    n    e
 
-    S -> 1, n = 1                        |      S -> 1, n = 1
-    s    n                               |      s    n   
-                                         |           e
+    S -> 1, n = 1                        |      S -> 1 -> NULL, n = 1
+    s    n                               |           s    n   
+         e                               |                e
          
     S -> 1 -> 2, n = 1                   |   S -> 1 -> 2, n = 1
     s    n                               |        s    n
